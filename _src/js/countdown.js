0ce7cd9e256062e3e515c7354d81385f9bdcab59
Rob Denton <robdentonrg@gmail.com>
2017-07-13 16:06:21 -0700

    Countdown work
    Add conditional to disappear after eclipse

--- _src/js/countdown.js
+++ _src/js/countdown.js
@@ -1,5 +1,6 @@
 var el = document.getElementById('countdown'),
 end = new Date('08/21/2017 10:16 AM'),
+//end = new Date('07/11/2017 4:05 PM'),
 _second = 1000, _minute = _second * 60, _hour = _minute * 60, _day = _hour * 24, timer;
 function showRemaining() {
 	//HTML
@@ -7,18 +8,18 @@ function showRemaining() {
 	
 	// Logic
 	var now = new Date(), distance = end - now;
-	var days = Math.floor(distance / _day),
-	hours = Math.floor((distance % _day) / _hour),
-	minutes = Math.floor((distance % _hour) / _minute),
-	seconds = Math.floor((distance % _minute) / _second);
-	if (days > 1){
-		el.innerHTML = head + '<h1>' + days + '</h1><h5>Days</h5>';
-	} else if (days == 1) {
-		el.innerHTML = head + '<h1>' + days + '</h1><h5>Day</h5>';
-	} else if (hours > 1) {
-		el.innerHTML = head + '<h1>' + hours + '</h1><h5>Hours</h5>';
-	} else {
-		el.innerHTML = head + '<h4>Less than<br>an hour!</h4>';
+	if (distance > 0){
+		var days = Math.floor(distance / _day),
+		hours = Math.floor((distance % _day) / _hour),
+		minutes = Math.floor((distance % _hour) / _minute),
+		seconds = Math.floor((distance % _minute) / _second);
+		if (days > 1){
+			el.innerHTML = head + '<h1>' + days + '</h1><h5>Days</h5>';
+		} else if (days == 1) {
+			el.innerHTML = head + '<h1>' + days + '</h1><h5>Day</h5>';
+		} else {
+			el.innerHTML = head + '<h4>Today is the<br>eclipse!</h4>';
+		}
 	}
 }
 showRemaining();
