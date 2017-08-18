96b125392aeaa23f4e88dcc61afd6b32b5e539f3
J. Heasly <jheasly@earthlink.net>
2017-08-17 18:34:33 -0700

    Tweaks for Storify widget.

--- _src/js/countdown.js
+++ _src/js/countdown.js
@@ -2,13 +2,14 @@ var el = document.getElementById('countdown'),
 end = new Date('08/21/2017 10:16 AM'),
 //end = new Date('07/13/2017 7:05 PM'),
 _second = 1000, _minute = _second * 60, _hour = _minute * 60, _day = _hour * 24, timer;
+
 function showRemaining() {
 	//HTML
 	head = '<div class="label">Countdown</div>';
 	
 	// Logic
 	var now = new Date(), distance = end - now;
-	if (distance > 0){
+	if (distance > 0 && el !== null){
 		var days = Math.floor(distance / _day),
 		hours = Math.floor((distance % _day) / _hour),
 		minutes = Math.floor((distance % _hour) / _minute),
